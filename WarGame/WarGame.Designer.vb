13dda43167bac472c3e5a98a71ae286567670718
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-10-03 18:39:20 -0600

    Round Winner function added Not allowing to restart a game during one added

--- WarGame/WarGame.Designer.vb
+++ WarGame/WarGame.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class WarGame
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class WarGame
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.ThrowButton = New System.Windows.Forms.Button()
         Me.Card1TextBox = New System.Windows.Forms.TextBox()
@@ -28,6 +28,8 @@ Partial Class WarGame
         Me.ResultTextBox = New System.Windows.Forms.TextBox()
         Me.RoundsTextBox = New System.Windows.Forms.TextBox()
         Me.NewGameButton = New System.Windows.Forms.Button()
+        Me.Player1WinsTextBox = New System.Windows.Forms.TextBox()
+        Me.Player2WinsTextBox = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'ThrowButton
@@ -41,30 +43,30 @@ Partial Class WarGame
         '
         'Card1TextBox
         '
-        Me.Card1TextBox.Location = New System.Drawing.Point(56, 35)
+        Me.Card1TextBox.Location = New System.Drawing.Point(56, 360)
         Me.Card1TextBox.Name = "Card1TextBox"
-        Me.Card1TextBox.Size = New System.Drawing.Size(100, 22)
+        Me.Card1TextBox.Size = New System.Drawing.Size(176, 22)
         Me.Card1TextBox.TabIndex = 1
         '
         'Card2TextBox
         '
-        Me.Card2TextBox.Location = New System.Drawing.Point(56, 74)
+        Me.Card2TextBox.Location = New System.Drawing.Point(420, 360)
         Me.Card2TextBox.Name = "Card2TextBox"
-        Me.Card2TextBox.Size = New System.Drawing.Size(100, 22)
+        Me.Card2TextBox.Size = New System.Drawing.Size(176, 22)
         Me.Card2TextBox.TabIndex = 2
         '
         'ResultTextBox
         '
-        Me.ResultTextBox.Location = New System.Drawing.Point(56, 124)
+        Me.ResultTextBox.Location = New System.Drawing.Point(238, 360)
         Me.ResultTextBox.Name = "ResultTextBox"
-        Me.ResultTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.ResultTextBox.Size = New System.Drawing.Size(176, 22)
         Me.ResultTextBox.TabIndex = 3
         '
         'RoundsTextBox
         '
-        Me.RoundsTextBox.Location = New System.Drawing.Point(56, 162)
+        Me.RoundsTextBox.Location = New System.Drawing.Point(238, 388)
         Me.RoundsTextBox.Name = "RoundsTextBox"
-        Me.RoundsTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.RoundsTextBox.Size = New System.Drawing.Size(176, 22)
         Me.RoundsTextBox.TabIndex = 4
         '
         'NewGameButton
@@ -76,11 +78,27 @@ Partial Class WarGame
         Me.NewGameButton.Text = "New Game"
         Me.NewGameButton.UseVisualStyleBackColor = True
         '
+        'Player1WinsTextBox
+        '
+        Me.Player1WinsTextBox.Location = New System.Drawing.Point(56, 388)
+        Me.Player1WinsTextBox.Name = "Player1WinsTextBox"
+        Me.Player1WinsTextBox.Size = New System.Drawing.Size(176, 22)
+        Me.Player1WinsTextBox.TabIndex = 6
+        '
+        'Player2WinsTextBox
+        '
+        Me.Player2WinsTextBox.Location = New System.Drawing.Point(420, 388)
+        Me.Player2WinsTextBox.Name = "Player2WinsTextBox"
+        Me.Player2WinsTextBox.Size = New System.Drawing.Size(176, 22)
+        Me.Player2WinsTextBox.TabIndex = 7
+        '
         'WarGame
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Player2WinsTextBox)
+        Me.Controls.Add(Me.Player1WinsTextBox)
         Me.Controls.Add(Me.NewGameButton)
         Me.Controls.Add(Me.RoundsTextBox)
         Me.Controls.Add(Me.ResultTextBox)
@@ -100,4 +118,6 @@ Partial Class WarGame
     Friend WithEvents ResultTextBox As TextBox
     Friend WithEvents RoundsTextBox As TextBox
     Friend WithEvents NewGameButton As Button
+    Friend WithEvents Player1WinsTextBox As TextBox
+    Friend WithEvents Player2WinsTextBox As TextBox
 End Class
--- WarGame/WarGame.vb
+++ WarGame/WarGame.vb
@@ -1,17 +1,29 @@
 ﻿
 Public Class WarGame
+
     Private deck As Deck
     Private player1Deck As List(Of Card)
     Private player2Deck As List(Of Card)
     Private random As New Random()
     Private roundCounter As Integer = 0 ' Initialize the round counter
+    Private player1Wins As Integer = 0 ' Count Player 1's wins
+    Private player2Wins As Integer = 0 ' Count Player 2's wins
+    Private player1GameWins As Integer = 0 ' Count Player 1's game wins
+    Private player2GameWins As Integer = 0 ' Count Player 2's game wins
+    Private isGameInProgress As Boolean = False ' Track if a game is in progress
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Initialize the deck and shuffle
         InitializeGame()
     End Sub
 
-    Private Sub btnDrawCards_Click(sender As Object, e As EventArgs) Handles ThrowButton.Click
+    Private Sub ThrowButton_Click(sender As Object, e As EventArgs) Handles ThrowButton.Click
+        ' Check if a game is in progress
+        If Not isGameInProgress Then
+            MessageBox.Show("Please start a new game before drawing cards.")
+            Return
+        End If
+
         ' Draw one card for each player
         Dim player1Card As Card = DrawCardForPlayer(player1Deck)
         Dim player2Card As Card = DrawCardForPlayer(player2Deck)
@@ -28,8 +40,12 @@ Public Class WarGame
 
             If player1Value > player2Value Then
                 ResultTextBox.Text = "Player 1 wins!"
+                player1Wins += 1 ' Increment Player 1's win counter
+                Player1WinsTextBox.Text = "Player 1 Wins: " & player1Wins.ToString() ' Update Player 1's win count
             ElseIf player1Value < player2Value Then
                 ResultTextBox.Text = "Player 2 wins!"
+                player2Wins += 1 ' Increment Player 2's win counter
+                Player2WinsTextBox.Text = "Player 2 Wins: " & player2Wins.ToString() ' Update Player 2's win count
             Else
                 ResultTextBox.Text = "It's a tie!"
             End If
@@ -37,46 +53,79 @@ Public Class WarGame
             ' Increment the round counter and display it
             roundCounter += 1
             RoundsTextBox.Text = "Rounds: " & roundCounter.ToString()
+
         Else
-            ' If one or both players have no more cards, end the game
+            ' If one or both players have no more cards, determine the game winner
+            DetermineGameWinner()
+
+            ' End the game and show game over message
             ResultTextBox.Text = "No more cards to draw. Game over!"
+            isGameInProgress = False ' Mark the game as not in progress
         End If
     End Sub
 
     ' Helper function to draw a card from a player's deck
     Private Function DrawCardForPlayer(playerDeck As List(Of Card)) As Card
-        If playerDeck.Count > 0 Then
-            Dim cardIndex As Integer = random.Next(playerDeck.Count)
-            Dim card As Card = playerDeck(cardIndex)
-            playerDeck.RemoveAt(cardIndex) ' Remove the drawn card from the player's deck
-            Return card
-        Else
-            Return Nothing ' Return nothing if the deck is empty
-        End If
-    End Function
+            If playerDeck.Count > 0 Then
+                Dim cardIndex As Integer = random.Next(playerDeck.Count)
+                Dim card As Card = playerDeck(cardIndex)
+                playerDeck.RemoveAt(cardIndex) ' Remove the drawn card from the player's deck
+                Return card
+            Else
+                Return Nothing ' Return nothing if the deck is empty
+            End If
+        End Function
 
     ' Reset game when reset button is clicked
-    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles NewGameButton.Click
+    Private Sub NewGameButton_Click(sender As Object, e As EventArgs) Handles NewGameButton.Click
+        If isGameInProgress Then
+            MessageBox.Show("Please finish the current game before starting a new one.")
+            Return
+        End If
         InitializeGame() ' Call the method to initialize a new game
     End Sub
 
-    ' Method to reset the game state
+    ' Method to reset the game state but keep track of game wins and total games played
     Private Sub InitializeGame()
-        ' Initialize a new deck and shuffle it
-        deck = New Deck()
-        deck.Shuffle()
+            ' Initialize a new deck and shuffle it
+            deck = New Deck()
+            deck.Shuffle()
 
-        ' Split the deck into two halves, 26 cards for each player
-        player1Deck = deck.GetCards(26) ' First half for Player 1
-        player2Deck = deck.GetCards(26) ' Second half for Player 2
+            ' Split the deck into two halves, 26 cards for each player
+            player1Deck = deck.GetCards(26) ' First half for Player 1
+            player2Deck = deck.GetCards(26) ' Second half for Player 2
 
-        ' Reset the round counter
-        roundCounter = 0
+            ' Reset the round counter and win counters for the new game
+            roundCounter = 0
+            player1Wins = 0
+            player2Wins = 0
+
+        ' Update the display
         RoundsTextBox.Text = "Rounds: 0"
+        Player1WinsTextBox.Text = "Player 1 Wins: 0"
+        Player2WinsTextBox.Text = "Player 2 Wins: 0"
 
         ' Clear the textboxes for player cards and result
         Card1TextBox.Clear()
         Card2TextBox.Clear()
         ResultTextBox.Clear()
-    End Sub
+
+        ' Set the game in progress flag to true
+        isGameInProgress = True
+        End Sub
+
+        ' Determine which player won the game based on the number of rounds won
+        Private Sub DetermineGameWinner()
+            If player1Wins > player2Wins Then
+                player1GameWins += 1 ' Player 1 wins the game
+            Player1WinsTextBox.Text = "Player 1 Game Wins: " & player1GameWins.ToString()
+            ResultTextBox.Text = "Player 1 wins the game!"
+        ElseIf player1Wins < player2Wins Then
+                player2GameWins += 1 ' Player 2 wins the game
+            Player2WinsTextBox.Text = "Player 2 Game Wins: " & player2GameWins.ToString()
+            ResultTextBox.Text = "Player 2 wins the game!"
+        Else
+            ResultTextBox.Text = "The game is a tie!"
+        End If
+        End Sub
 End Class
